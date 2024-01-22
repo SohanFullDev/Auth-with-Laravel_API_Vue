beb3a1d6f42530b371d6b018f60d74076f92cd64
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-01-22 15:20:44 +0600

    controller_api test_b

--- laravel-auth-api/routes/api.php
+++ laravel-auth-api/routes/api.php
@@ -20,7 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', [LoginController::class, 'check']);
-Route::post('show/{id}', [LoginController::class, 'show']);
+
+//Route::post('show/{id}', [LoginController::class, 'show']);
 
 Route::post('/register', [RegisterController::class, 'store']);
+Route::post('/login', [LoginController::class, 'check']);
